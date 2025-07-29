cc3109c966c8f9e945eef9fe31f7bbbabdffe567
Amal Irfan <69129408+AmalIrfan@users.noreply.github.com>
2025-07-30 02:29:32 +0530

    Track dependecies

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        # list dependencies
+        'psutil>=7.0.0'
     ],
+    python_requires='>=3.10',
     author='DBCM Technologies',
     description='A HTTP File Server',
     url='https://github.com/DBCM-Technologies/df',
